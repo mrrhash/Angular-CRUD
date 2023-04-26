8a9c3119a567d68ee875f88fdb3044f74b7d912c
mrrhash <hr38359@gmail.com>
2023-04-26 22:47:18 +0500

    working on app-structure

---
+++ PaymentApp/src/app/payment-detail/payment-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail',
+  templateUrl: './payment-detail.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailComponent {
+
+}
